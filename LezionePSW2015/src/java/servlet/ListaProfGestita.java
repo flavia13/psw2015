3e3ea20e5476160a9803e0460d458cf059920503
Sergio.Flesca@gmail.com <Sergio.Flesca@gmail.com@87af9141-69e9-1db4-b5df-2628fb83d347>
2015-03-19 10:57:16 +0000

    da portatile

---
+++ LezionePSW2015/src/java/servlet/ListaProfGestita.java
@@ -0,0 +1,119 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.annotation.Resource;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+
+/**
+ *
+ * @author sflesca
+ */
+public class ListaProfGestita extends HttpServlet {
+    @Resource(name = "esempioDB2015DS")
+    private DataSource esempioDB2015DS;
+    
+    
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        Connection con =null;
+        try {
+            response.setContentType("text/html;charset=UTF-8");
+            con = esempioDB2015DS.getConnection();
+            try (PrintWriter out = response.getWriter()) {
+                /* TODO output your page here. You may use following sample code. */
+                out.println("<!DOCTYPE html>");
+                out.println("<html>");
+                out.println("<head>");
+                out.println("<title>Servlet ListaProfessori</title>");
+                out.println("</head>");
+                out.println("<body>");
+                out.println("<h1>ListaProfessori</h1>");
+                Statement st = con.createStatement();
+                ResultSet rs = st.executeQuery("select * from Professore");
+                while(rs.next()){
+                    out.println("<p><a href=\"ListaCorsi?PID="+rs.getInt(1)+"\" >Prof:"+rs.getInt(1)+"</a> Cognome:"+rs.getString(2)+" Nome:"
+                            +rs.getString(3)+" Settore:"+rs.getString(4));
+                }
+                out.println("</body>");
+                out.println("</html>");
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(ListaProfessori.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            if(con!=null)
+                try {
+                    con.close();
+            } catch (SQLException ex) {
+                Logger.getLogger(ListaProfessori.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
